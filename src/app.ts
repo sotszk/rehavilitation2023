1b5c9c040e867a95aa6433fe1c6021b21c8a633e
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-01-29 19:45:19 +0900

    ナビゲーションまわりのキャッチアップを兼ねて

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,35 @@ new Typed('.typed', {
   typeSpeed: 40,
   showCursor: false,
 });
+
+const menuOpenerEl = document.getElementById('menu-opener');
+const menuEl = document.getElementById('menu');
+const mainEl = document.getElementById('main');
+
+const toggleMenu = () => {
+  const currentState = menuEl.classList.contains('is-visible')
+    ? 'open'
+    : 'close';
+
+  if (currentState === 'open') {
+    menuEl.classList.remove('is-visible');
+    mainEl.classList.remove('is-translated');
+  } else {
+    menuEl.classList.add('is-visible');
+    mainEl.classList.add('is-translated');
+  }
+};
+
+menuOpenerEl.addEventListener('click', (evt) => {
+  toggleMenu();
+  evt.stopPropagation();
+  evt.preventDefault();
+});
+
+mainEl.addEventListener('click', (evt) => {
+  const isClickable = mainEl.classList.contains('is-translated');
+  if (isClickable) {
+    toggleMenu();
+    evt.preventDefault();
+  }
+});
